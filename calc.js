106fd8ea1116072d7d25b5520e48223b7e7d36e2
Aleksandra <94486752+taaleksandra@users.noreply.github.com>
2023-10-12 15:00:29 +0200

    poprawki od klienta

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 const creditForm = document.querySelector(".calc-form");
 
 // powiadomienia formularza
-const validMessPer = document.querySelector("#valid-mess");
+// const validMessPer = document.querySelector("#valid-mess");
 const validMessPerSec = document.querySelector("#valid-mess-sec");
 const validMessPerc = document.querySelector("#valid-mess-perc");
 const validMessNumb = document.querySelector("#valid-mess-numb");
@@ -41,17 +41,24 @@ function PMT(ir, np, pv, fv, type) {
 
 const handleSubmit = (e) => {
   e.preventDefault();
+
   succesInfo.classList.remove("calc-mess-visible");
+  validMessNumb.classList.remove("calc-mess-visible");
+  validMessPerSec.classList.remove("calc-mess-visible");
+
   paramValue.textContent = 0 + " PLN";
-  paramPerc.textContent = 0 + " %";
+  // paramPerc.textContent = 0 + " %";
   paramPerdiod.textContent = 0;
   result.textContent = 0;
 
+  const creditInterest = 7.14;
+
   const form = e.target;
   const creditValue = form.elements.value.value;
-  const creditInterest = form.elements.interest.value;
-  const creditMonthPer = form.elements.month.value;
+  // const creditInterest = form.elements.interest.value;
+  // const creditMonthPer = form.elements.month.value;
   const creditYearPer = form.elements.year.value;
+  const credit2 = form.elements.credit2.checked;
 
   // ----------------------------------------------------------------
   //   VALIDATION
@@ -59,36 +66,32 @@ const handleSubmit = (e) => {
   if (Number.isNaN(Number(creditValue)) || creditValue === "") {
     validMessNumb.classList.add("calc-mess-visible");
     return;
-  } else if (Number.isNaN(Number(creditMonthPer))) {
-    validMessNumb.classList.add("calc-mess-visible");
-    return;
+    // } else if (Number.isNaN(Number(creditMonthPer))) {
+    //   validMessNumb.classList.add("calc-mess-visible");
+    //   return;
   } else if (Number.isNaN(Number(creditYearPer))) {
     validMessNumb.classList.add("calc-mess-visible");
     return;
-  } else {
-    validMessNumb.classList.remove("calc-mess-visible");
   }
 
-  if (creditYearPer === "" && creditMonthPer === "") {
+  if (creditYearPer === "") {
     validMessPerSec.classList.add("calc-mess-visible");
     return;
-  } else {
-    validMessPerSec.classList.remove("calc-mess-visible");
   }
 
-  if (creditYearPer !== "" && creditMonthPer !== "") {
-    validMessPer.classList.add("calc-mess-visible");
-    return;
-  } else {
-    validMessPer.classList.remove("calc-mess-visible");
-  }
+  // if (creditYearPer !== "" && creditMonthPer !== "") {
+  //   validMessPer.classList.add("calc-mess-visible");
+  //   return;
+  // } else {
+  //   validMessPer.classList.remove("calc-mess-visible");
+  // }
 
-  if (Number.isNaN(Number(creditInterest)) || creditInterest === "") {
-    validMessPerc.classList.add("calc-mess-visible");
-    return;
-  } else {
-    validMessPerc.classList.remove("calc-mess-visible");
-  }
+  // if (Number.isNaN(Number(creditInterest)) || creditInterest === "") {
+  //   validMessPerc.classList.add("calc-mess-visible");
+  //   return;
+  // } else {
+  //   validMessPerc.classList.remove("calc-mess-visible");
+  // }
 
   // ----------------------------------------------------------------
   //   WYLICZENIE RATY
@@ -96,31 +99,44 @@ const handleSubmit = (e) => {
   const formattedValue = Number(creditValue)
     .toLocaleString(undefined, {
       useGrouping: true,
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
     })
     .replace(",", " ");
 
-  let creditPeriod = Number(creditMonthPer);
-  if (creditMonthPer === "") {
-    creditPeriod = Number(creditYearPer) * 12;
+  // let creditPeriod = Number(creditMonthPer);
+  // if (creditMonthPer === "") {
+  //   creditPeriod = Number(creditYearPer) * 12;
+  // }
+  const creditPeriod = Number(creditYearPer) * 12;
+
+  if (!credit2) {
+    const ir = Number(creditInterest) / 100 / 12;
+
+    const pmt = PMT(ir, creditPeriod, Number(creditValue)).toFixed(2);
+    if (!Number.isNaN(Number(pmt))) {
+      const roundResult = Math.round(pmt);
+      const formattedResult = Math.abs(roundResult)
+        .toLocaleString(undefined, {
+          useGrouping: true,
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        })
+        .replace(",", " ");
+
+      result.textContent = formattedResult;
+    } else {
+      result.textContent = 0;
+    }
+
+    paramValue.textContent = formattedValue + " zł";
+    // paramPerc.textContent = creditInterest + " %";
+    paramPerc.textContent = "6.9 %";
+    paramPerdiod.textContent = creditYearPer;
+  } else {
+    // TUTAJ DODAĆ LOGIKĘ KREDYTU 2%
+    console.log("DODAJ LOGIKĘ KREDUTY 2%, ABY WYLICZYĆ RATĘ :)))");
   }
-
-  const ir = Number(creditInterest) / 100 / 12;
-
-  const pmt = PMT(ir, creditPeriod, Number(creditValue)).toFixed(2);
-  const formattedResult = Math.abs(pmt)
-    .toLocaleString(undefined, {
-      useGrouping: true,
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    })
-    .replace(",", " ");
-
-  paramValue.textContent = formattedValue + " PLN";
-  paramPerc.textContent = creditInterest + " %";
-  paramPerdiod.textContent = creditPeriod;
-  result.textContent = formattedResult;
 
   succesInfo.classList.add("calc-mess-visible");
 
